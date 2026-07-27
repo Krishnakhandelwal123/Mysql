bfe6a2f5ba511bb329a947564affe63fed9fc70e
krishna khandelwal <141343323+Krishnakhandelwal123@users.noreply.github.com>
2026-07-27 14:48:33 +0530

    Time: 562 ms (89.69%), Space: 0B (100%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,4 @@
+select date_format(trans_date,'%Y-%m') as month,country,count(id) AS trans_count,SUM(state='approved') as approved_count,sum(amount) as trans_total_amount,
+sum( if(state='approved',amount,0)) as approved_total_amount
+from Transactions
+group by month,country
