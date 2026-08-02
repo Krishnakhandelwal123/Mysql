cb52ebeac72a9254ee96c62ffadb9f3f46876b86
krishna khandelwal <141343323+Krishnakhandelwal123@users.noreply.github.com>
2026-08-02 18:05:40 +0530

    Time: 591 ms (46.76%), Space: 0B (100%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,6 @@
+    select employee_id,department_id
+    from Employee
+    where employee_id in  (select employee_id
+    from employee
+    group by employee_id
+    having count(*)=1 ) or primary_flag='y'
