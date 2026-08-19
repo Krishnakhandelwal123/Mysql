79d8d90db8378dce90b30872c816cbd946b9bbf2
krishna khandelwal <141343323+Krishnakhandelwal123@users.noreply.github.com>
2026-08-19 11:14:48 +0530

    Time: 0 ms (100%), Space: 9.2 MB (62.87%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int>freq(26,0);
+        for(auto c:s){
+            freq[c-'a']++;
+        }
+        vector<bool>used(26,false);
+
+        string st;
+         for(char c : s) {
+
+            freq[c - 'a']--;
+
+            if(used[c - 'a'])
+                continue;
+
+            while(!st.empty() &&
+                  st.back() > c &&
+                  freq[st.back() - 'a'] > 0) {
+
+                used[st.back() - 'a'] = false;
+                st.pop_back();
+            }
+
+            st.push_back(c);
+            used[c - 'a'] = true;
+        }
+        return st;
+    }
+};
